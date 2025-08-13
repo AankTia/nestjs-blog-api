0a883a3c5271fc021d2c264dd1086f3818e18f78
TiaWidi <tia.septianawidi@gmail.com>
2025-08-13 10:36:36 +0700

    Add missing import validator

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @ApiProperty({ example: 'user@example.com' })
