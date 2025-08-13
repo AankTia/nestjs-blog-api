2be2732093cacd4d92624cb967dcda751d03d1ab
TiaWidi <tia.septianawidi@gmail.com>
2025-08-13 21:06:43 +0700

    Add missing import

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { UploadsController } from './uploads.controller';
+import { UploadsService } from './uploads.service';
 
 @Module({
   controllers: [UploadsController],
