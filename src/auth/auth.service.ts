911070ab8b24893d99d6517bf89b6d247f7b4ea7
TiaWidi <tia.septianawidi@gmail.com>
2025-08-13 10:41:59 +0700

    Fix auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { RegisterDto } from './dto/register.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
@@ -9,7 +9,7 @@ export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
@@ -24,15 +24,15 @@ export class AuthService {
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
-      user
+      user,
     };
   }
 
   async register(registerDto: RegisterDto) {
-    const hashedPassword = await bcrypt.hash(RegisterDto.password, 10);
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
     const user = await this.usersService.create({
-      ...RegisterDto,
-      password: hashedPassword
+      ...registerDto,
+      password: hashedPassword,
     });
     const { password, ...result } = user;
     return this.login(result);
