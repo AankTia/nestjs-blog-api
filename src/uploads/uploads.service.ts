2d64625ad531758da3bc5c8f0877140929323201
TiaWidi <tia.septianawidi@gmail.com>
2025-08-13 21:11:25 +0700

    Add missing import

--- src/uploads/uploads.service.ts
+++ src/uploads/uploads.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import path from 'path';
+import fs from 'fs/promises';
 
 @Injectable()
 export class UploadsService {
