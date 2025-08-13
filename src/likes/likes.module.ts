b1f9b85dcdb1408fa66e286ba3cef5b7ebd56af9
TiaWidi <tia.septianawidi@gmail.com>
2025-08-13 20:51:41 +0700

    Implement like module

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from 'src/posts/posts.module';
+import { Like } from 'typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Like]), PostsModule],
   controllers: [LikesController],
   providers: [LikesService],
+  exports: [LikesService],
 })
 export class LikesModule {}
